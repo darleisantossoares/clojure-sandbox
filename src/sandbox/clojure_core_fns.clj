938a830514a0df8d85b04cb86e020dae60e2641d
Darlei Soares <darlei.soares@nubank.com.br>
2024-02-08 09:14:41 -0300

    add 50 fns

--- src/sandbox/clojure_core_fns.clj
+++ src/sandbox/clojure_core_fns.clj
@@ -3,3 +3,40 @@
 (println "...")
 
 
+;; vectors
+[1 2 3 :a "asdf" [:a 1]]
+;; sets
+#{1 2 3}
+;; maps
+{:key "value"}
+;; lists
+'(1 2 3 2 1)
+;; sequences
+(seq [1 2 3])
+;; empty?
+(empty? #{})
+;; get
+(get [1 2 3] 0)
+(get {:first "Darlei" :second "Soares" "qqcoisa" "ttl"} "qqcoisa")
+;; get in -> get into nested maps and vectors
+;; nth
+;; assoc
+;; assoc-in
+;; dissoc
+;; update
+;; update-in
+;; merge
+;; merge with
+;; count
+;; sum
+;; apply
+;; first
+;; second
+;; nth
+;; rest
+;; last
+;; but last
+
+
+
+
